3a6fbe37ef98fca9f05c8b945d5e0b9464486bc9
Ivan Ugarte <ivanugartem@gmail.com>
2021-02-24 16:23:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+ const getNombre = (idPost) => {
+
+  fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+    .then(res => {
+      return res.json()
+    })
+    .then(post => {
+      console.log(post.userId)
+      fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+        .then(res => {
+          return res.json()
+        })
+        .then(user => {
+          console.log(user.name)
+        })
+    })
+  };
+
+  getNombre(7);
+
+  // llamada con fetch (async, await)
+
+  const getNombreAsync = async (idPost) => {
+
+    try {
+      const resPost = await  fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+      const post = await resPost.json()
+      console.log(post)
+      const resUser = await   fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+      const user = await resUser.json()
+      console.log(user);  
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  getNombreAsync(80);
+
+// llamada con Axios (async,await) 
+
+  const getNombreAxios = async (idPost) => {
+
+    try {
+      const resPost = await axios(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+      const resUser = await axios(`https://jsonplaceholder.typicode.com/users/${resPost.data.userId}`)
+      console.log(resPost.data.userId);  
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  getNombreAxios(80);
